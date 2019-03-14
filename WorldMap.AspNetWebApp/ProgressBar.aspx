2ae24e2464888ed8764a1f057905839edc02a8fb
mitesh90 <miteshpatel6716@gmail.com>
2019-03-14 16:05:47 -0500

    Added Progressbar logic in Asp.net project

---
+++ WorldMap.AspNetWebApp/ProgressBar.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProgressBar.aspx.cs" Inherits="WorldMap.AspNetWebApp.ProgressBar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server" EnablePartialRendering="True">
+            </asp:ScriptManager>
+            <div>
+                <asp:Timer ID="TimerControlNew" runat="server" OnTick="TimerControl1_Tick" Enabled="false" Interval="2000">
+                </asp:Timer>
+            </div>
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server" Mode="Conditional">
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="TimerControlNew" EventName="Tick" />
+                </Triggers>
+                <ContentTemplate>
+                    <div style="background-color: coral; height: 30px; width: 300px">
+                        <div id="bar" runat="server"
+                            style="height: 30px; width: 0px; background-color: cadetblue">
+                        </div>
+                    </div>
+
+                    <asp:Label ID="Label1" runat="server" Text="0 %"></asp:Label>
+                    <br />
+                    <br />
+                    <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/Default.aspx" Visible="False">Reload Page</asp:HyperLink><br />
+                </ContentTemplate>
+            </asp:UpdatePanel>
+
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click1" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WorldMap.AspNetWebApp/ProgressBar.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading;
+using System.Web.UI.HtmlControls;
+
+namespace WorldMap.AspNetWebApp
+{
+    public partial class ProgressBar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DoTheWork()
+        {
+            int i = 0;
+            while (i < 100)
+            {
+                Thread.Sleep(1000);
+                i = i + 1;
+                Session["status"] = i;
+            }
+            Thread objThread = (Thread)Session["Thread"];
+            objThread.Abort();
+        }
+
+        protected void TimerControl1_Tick(object sender, System.EventArgs e)
+        {
+            int i = (int)Session["status"];
+            if (i == 100)
+            {
+                TimerControlNew.Enabled = false;
+                HyperLink1.Visible = true;
+            }
+            Label1.Text = i.ToString() + "%";
+            HtmlGenericControl div = (HtmlGenericControl)this.Controls[0].FindControl("bar");
+            i = i * 3;
+            string p = (i.ToString() + "px");
+            div.Style.Add("width", p);
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            Session.Add("status", 0);
+            Thread objThread = new Thread(new System.Threading.ThreadStart(DoTheWork));
+            objThread.IsBackground = true;
+            objThread.Start();
+            Session["Thread"] = objThread;
+            Button1.Enabled = false;
+            TimerControlNew.Enabled = true;
+        }
+    }
+}
